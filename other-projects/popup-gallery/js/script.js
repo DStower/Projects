3c9be610732fcfa691b3cd1f8fe132560c5a6e42
DStower <108472044+DStower@users.noreply.github.com>
2022-08-31 18:50:12 -0500

    Add files via upload

---
+++ other-projects/popup-gallery/js/script.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('.image-container img').forEach(image =>{
+    image.onclick = () =>{
+        document.querySelector('.popup-image').style.display = 'block';
+        document.querySelector('.popup-image img').src = image.getAttribute('src');
+    }
+});
+
+window.addEventListener('load', () =>{
+    document.querySelector('.popup-image').style.display = 'none';
+});
+
+document.querySelector('.popup-image span').onclick = () =>{
+    document.querySelector('.popup-image').style.display = 'none';
+}
